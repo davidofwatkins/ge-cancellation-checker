cb853323145baa0b347aa89d8ed0767da6f319a1
David Watkins <dwat91@gmail.com>
2015-05-02 00:53:16 -0700

    Initial commit for GOES Global Entry appointment cancellation checker

---
+++ goes-checker-cron.py
@@ -0,0 +1,84 @@
+#!/usr/bin/python
+
+# Note: for setting up email with sendmail, see: http://linuxconfig.org/configuring-gmail-as-sendmail-email-relay
+
+from subprocess import check_output
+from datetime import datetime
+from os import path
+import sys, smtplib, json
+
+PWD = path.dirname(sys.argv[0]) 
+
+# Get settings
+try:
+    with open('%s/goes-config.json' % PWD) as json_file:    
+        settings = json.load(json_file)
+except Exception as e:
+    print 'Error extracting config file: %s' % e
+    sys.exit()
+
+# Make sure we have all our settings
+if not 'current_interview_date_str' in settings or not settings['current_interview_date_str']:
+    print 'Missing current_interview_date_str in config'
+    sys.exit()
+if not 'email_from' in settings or not settings['email_from']:
+    print 'Missing from address in config'
+    sys.exit()
+if not 'email_to' in settings or not settings['email_to']:
+    print 'Missing to address in config'
+    sys.exit()
+if not 'init_url' in settings or not settings['init_url']:
+    print 'Missing initial URL in config'
+    sys.exit()
+if not 'username' in settings or not settings['username']:
+    print 'Missing current_interview_date_str in config'
+    sys.exit()
+if not 'password' in settings or not settings['password']:
+    print 'Missing current_interview_date_str in config'
+    sys.exit()
+
+CURRENT_INTERVIEW_DATE = datetime.strptime(settings['current_interview_date_str'], '%B %d, %Y')
+
+def log(msg):
+    print msg
+
+    if not 'logfile' in settings or not settings['logfile']: return
+    with open(settings['logfile'], 'a') as logfile:
+        logfile.write('%s: %s\n' % (datetime.now(), msg))
+
+def send_apt_available_email(current_apt, avail_apt):
+    message = """From: %s
+To: %s
+Subject: Alert: New Global Entry Appointment Available
+Content-Type: text/html
+
+<p>Good news! There's a new Global Entry appointment available on <b>%s</b> (your current appointment is on %s).</p>
+
+<p>If this sounds good, please sign in to https://goes-app.cbp.dhs.gov/main/goes to reschedule.</p>
+
+<p>If you reschedule, please remember to update CURRENT_INTERVIEW_DATE in your goes-config.json file.</p>
+""" % (settings['email_from'], ', '.join(settings['email_to']), avail_apt.strftime('%B %d, %Y'), current_apt.strftime('%B %d, %Y'))
+
+    try:
+        server = smtplib.SMTP('localhost')
+        server.sendmail(settings['email_from'], settings['email_to'], message)
+        server.quit()
+    except Exception as e:
+        log('Failed to send success email')
+
+
+
+# new_apt_str = check_output(['phantomjs', '%s/goes-checker.js' % PWD]); # get string from PhantomJS script - formatted like 'July 20, 2015'
+new_apt_str = 'May 2, 2015'
+new_apt_str = new_apt_str.strip()
+
+try: new_apt = datetime.strptime(new_apt_str, '%B %d, %Y')
+except ValueError as e:
+    log('%s' % new_apt_str)
+    sys.exit()
+
+if new_apt < CURRENT_INTERVIEW_DATE: # new appointment is newer than existing!
+    send_apt_available_email(CURRENT_INTERVIEW_DATE, new_apt)   
+    log('Found new appointment on %s (current is on %s)!' % (new_apt, CURRENT_INTERVIEW_DATE))
+else:
+    log('No new appointments. Next available on %s (current is on %s)' % (new_apt, CURRENT_INTERVIEW_DATE))
---
+++ goes-checker.js
@@ -0,0 +1,181 @@
+
+// CLI usage:
+// phantomjs [--ssl-protocol=any] goes-checker.js [-v|--verbose]
+
+var system = require('system');
+var fs = require('fs');
+
+var VERBOSE = false;
+var loadInProgress = false;
+
+// Calculate path of this file
+var PWD = '';
+var current_path_arr = system.args[0].split('/');
+if (current_path_arr.length == 1) { PWD = '.'; }
+else {
+    current_path_arr.pop();
+    PWD = current_path_arr.join('/');
+}
+
+// Gather Settings...
+var settings = JSON.parse(fs.read(PWD + '/goes-config.json'));
+if (!settings.username || !settings.username || !settings.init_url) {
+    console.log('Missing username, password, and/or initial URL. Exiting...');
+    phantom.exit();
+}
+
+// ...from command
+system.args.forEach(function(val, i) {
+    if (val == '-v' || val == '--verbose') { VERBOSE = true; }
+});
+
+function fireClick(el) {
+    var ev = document.createEvent("MouseEvents");
+    ev.initEvent("click", true, true);
+    el.dispatchEvent(ev);
+}
+
+var page = require('webpage').create();
+
+page.onConsoleMessage = function(msg) {
+    if (!VERBOSE) { return; }
+    console.log(msg);
+};
+
+page.onError = function(msg, trace) {
+    if (!VERBOSE) { return; }
+    console.error('Error on page: ' + msg);
+}
+
+page.onCallback = function(query, msg) {
+    if (query == 'username') { return settings.username; }
+    if (query == 'password') { return settings.password; }
+    if (query == 'fireClick') {
+        return function() { return fireClick; } // @todo:david DON'T KNOW WHY THIS DOESN'T WORK! :( Just returns [Object object])
+    }
+    if (query == 'report-interview-time') {
+        if (VERBOSE) { console.log('Next available appointment is at: ' + msg); }
+        else { console.log(msg); }
+        return;  
+    }
+    if (query == 'fatal-error') {
+        console.log('Fatal error: ' + msg);
+        phantom.exit();
+    }
+    return null;
+}
+
+page.onLoadStarted = function() { loadInProgress = true; };
+page.onLoadFinished = function() { loadInProgress = false; };
+
+if (VERBOSE) { console.log('Please wait...'); }
+
+page.open(settings.init_url);
+var steps = [
+    function() { // Log in
+        page.evaluate(function() {
+            console.log('On GOES login page...');
+            document.querySelector('input[name=username]').value = window.callPhantom('username');
+            document.querySelector('input[name=password]').value = window.callPhantom('password');
+            document.querySelector('form[action="/pkmslogin.form"]').submit();
+            console.log('Logging in...');
+        });
+    },
+    function() { // Accept terms
+        page.evaluate(function() {
+            
+            function fireClick(el) {
+                var ev = document.createEvent("MouseEvents");
+                ev.initEvent("click", true, true);
+                el.dispatchEvent(ev);
+            }
+            
+            var $acceptTermsBtn = document.querySelector('a[href="/main/goes/HomePagePreAction.do"]');
+
+            if (!$acceptTermsBtn) {
+                return window.callPhantom('fatal-error', 'Unable to find terms acceptance button');
+            }
+
+            fireClick($acceptTermsBtn);
+            console.log('Accepting terms...');
+        });
+    },
+    function() { // main dashboard
+        page.evaluate(function() {
+
+            function fireClick(el) {
+                var ev = document.createEvent("MouseEvents");
+                ev.initEvent("click", true, true);
+                el.dispatchEvent(ev);
+            }
+            
+            var $manageAptBtn = document.querySelector('.bluebutton[name=manageAptm]');
+            if (!$manageAptBtn) {
+                return window.callPhantom('fatal-error', 'Unable to find Manage Appointment button');
+            }
+
+            fireClick($manageAptBtn);
+            console.log('Entering appointment management...');
+        });
+    },
+    function() {
+        page.evaluate(function() {
+
+            function fireClick(el) {
+                var ev = document.createEvent("MouseEvents");
+                ev.initEvent("click", true, true);
+                el.dispatchEvent(ev);
+            }
+            
+            var $rescheduleBtn = document.querySelector('input[name=reschedule]');
+    
+            if (!$rescheduleBtn) {
+                return window.callPhantom('fatal-error', 'Unable to find reschedule button. Is it after or less than 24 hrs before your appointment?');
+            }
+
+            fireClick($rescheduleBtn);
+            console.log('Entering rescheduling selection page...');
+        });
+    },
+    function() {
+        page.evaluate(function() {
+
+            function fireClick(el) {
+                var ev = document.createEvent("MouseEvents");
+                ev.initEvent("click", true, true);
+                el.dispatchEvent(ev);
+            }
+
+            document.querySelector('select[name=selectedEnrollmentCenter]').value = '5446'; // SFO!
+            fireClick(document.querySelector('input[name=next]'));
+            console.log('Choosing SFO...');
+        });
+    },
+    function() {
+
+        page.evaluate(function() {
+
+            // We made it! Now we have to scrape the page for the earliest available date
+            
+            var date = document.querySelector('.date table tr:first-child td:first-child').innerHTML;
+            var month_year = document.querySelector('.date table tr:last-child td:last-child div').innerHTML;
+
+            var full_date = month_year.replace(',', ' ' + date + ',');
+            // console.log('');
+            window.callPhantom('report-interview-time', full_date)
+            // console.log('The next available appointment is on ' + full_date + '.');
+        });
+    }
+];
+
+var i = 0;
+interval = setInterval(function() {
+    if (loadInProgress) { return; } // not ready yet...
+    if (typeof steps[i] != "function") {
+        return phantom.exit();
+    }
+
+    steps[i]();
+    i++;
+
+}, 100);
